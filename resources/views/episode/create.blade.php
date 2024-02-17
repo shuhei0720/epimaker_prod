b31b015de2a4efa5c5f3859b94ad0826033d381d
shuhei0720 <shuhei_0720@icloud.com>
2024-02-17 21:19:17 +0900

    episodeのカラムが縦に並ぶよう、create.blade.phpを修正

--- resources/views/episode/create.blade.php
+++ resources/views/episode/create.blade.php
@@ -11,9 +11,10 @@
                 <input type="text" name="title" class="w-auto py-2 border border-gray-300 rounded-md" id="title">
             </div>
 
-            <div class="w-full flex felx-col">
-                <label for="episode" class="font-semibold mt-4">足りない分を補足し、より面白くなるよう脚色してみよう。<br>(比喩や擬態法も入れてみよう)</label>
-                <textarea name="episode" class="w-auto py-2 border border-gray-300 rounded-md" id="body" cols="30" rows="10"></textarea>
+            <div class="w-full flex flex-col">
+                <label for="episode" class="font-semibold mt-4">足りない分を補足し、より面白くなるよう脚色してみよう。(比喩や擬態法も入れてみよう)</label>
+                <textarea name="episode" class="w-auto py-2 border border-gray-300 rounded-md" id="episode" cols="30" rows="10">
+                </textarea>
             </div>
 
             <x-primary-button class="mt-4">
