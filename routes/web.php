a9baa776ead80f77b8e09c296bb09946a7288dde
shuhei0720 <shuhei_0720@icloud.com>
2024-02-17 19:59:10 +0900

    テスト画面を表示する処理を追加

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/test', [TestController::class, 'test'])
+->name('test');
 
 require __DIR__.'/auth.php';
