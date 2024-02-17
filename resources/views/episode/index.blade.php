20832f00dc7b288afaec37ec1a590fbe59d4708a
shuhei0720 <shuhei_0720@icloud.com>
2024-02-18 00:42:22 +0900

    一覧画面を表示するindex.blade.phpを作成

---
+++ resources/views/episode/index.blade.php
@@ -0,0 +1,25 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            一覧表示
+        </h2>
+    </x-slot>
+    <div class="mx-auto px-6">
+        @foreach($episodes as $episode)
+        <div class="mt-4 p-8 bg-white w-full rounded-2xl">
+            <h1 class="p-4 text-lg font-semibold">
+                {{$episode->title}}
+            </h1>
+            <hr class="w-full">
+            <p class="mt-4 p-4">
+                {{$episode->episode}}
+            </p>
+            <div class="p-4 text-sm font-semibold">
+                <p>
+                    {{$episode->created_at}}
+                </p>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</x-app-layout>
