5c0434aad0e86ed70a6dae5dab22db13f42d5a7f
shuhei0720 <shuhei_0720@icloud.com>
2024-02-17 23:25:17 +0900

    すべらない話作成画面のviewを作成

--- resources/views/episode/create.blade.php
+++ resources/views/episode/create.blade.php
@@ -7,18 +7,72 @@
     <div class="max-w-7xl mx-auto px-6">
         <form>
             <div class="w-full flex flex-col">
-                <label for="title" class="font-semibold mt-4">タイトル</label>
+                <label for="title" class="font-semibold mt-4">■最近会った出来事を思い返してタイトルを考えてみよう！■<br><br>(遊び、ニュース、季節、友達、旅、健康、仕事、学校、家族、恋人、住居、食事、etc...)</label>
                 <input type="text" name="title" class="w-auto py-2 border border-gray-300 rounded-md" id="title">
             </div>
 
             <div class="w-full flex flex-col">
-                <label for="episode" class="font-semibold mt-4">足りない分を補足し、より面白くなるよう脚色してみよう。(比喩や擬態法も入れてみよう)</label>
-                <textarea name="episode" class="w-auto py-2 border border-gray-300 rounded-md" id="episode" cols="30" rows="10">
-                </textarea>
+                <label for="when" class="font-semibold mt-4">■出来事を5W1H1Dに整理してみよう！■<br><br>いつ？</label>
+                <input type="text" name="when" class="w-auto py-2 border border-gray-300 rounded-md" id="when">
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="who" class="font-semibold mt-4">だれが？</label>
+                <input type="text" name="who" class="w-auto py-2 border border-gray-300 rounded-md" id="who">
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="what" class="font-semibold mt-4">なにを？</label>
+                <input type="text" name="what" class="w-auto py-2 border border-gray-300 rounded-md" id="what">
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="do" class="font-semibold mt-4">どうした？</label>
+                <input type="text" name="do" class="w-auto py-2 border border-gray-300 rounded-md" id="do">
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="why" class="font-semibold mt-4">なぜ？</label>
+                <input type="text" name="why" class="w-auto py-2 border border-gray-300 rounded-md" id="why">
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="how" class="font-semibold mt-4">どのように？</label>
+                <input type="text" name="how" class="w-auto py-2 border border-gray-300 rounded-md" id="how">
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="point" class="font-semibold mt-4">この話で一番共感してほしい、または、一番面白いポイントは？</label>
+                <input type="text" name="point" class="w-auto py-2 border border-gray-300 rounded-md" id="point">
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="beginning" class="font-semibold mt-4">■起承転結にまとめて、フリとオチを作ってみよう！■<br><br>起</label>
+                <textarea name="beginning" class="w-auto py-2 border border-gray-300 rounded-md" id="beginning" cols="30" rows="2"></textarea>
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="development" class="font-semibold mt-4">承</label>
+                <textarea name="development" class="w-auto py-2 border border-gray-300 rounded-md" id="development" cols="30" rows="2"></textarea>
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="turnand" class="font-semibold mt-4">転</label>
+                <textarea name="turnand" class="w-auto py-2 border border-gray-300 rounded-md" id="turnand" cols="30" rows="2"></textarea>
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="conclusion" class="font-semibold mt-4">結</label>
+                <textarea name="conclusion" class="w-auto py-2 border border-gray-300 rounded-md" id="conclusion" cols="30" rows="2"></textarea>
+            </div>
+
+            <div class="w-full flex flex-col">
+                <label for="episode" class="font-semibold mt-4">■足りない分を補足し、より面白くなるよう脚色したら完成！(比喩や擬態法も入れてみよう)■</label>
+                <textarea name="episode" class="w-auto py-2 border border-gray-300 rounded-md" id="episode" cols="30" rows="10"></textarea>
             </div>
 
             <x-primary-button class="mt-4">
-                登録する
+                完成！
             </x-primary-button>
         </form>
     </div>
