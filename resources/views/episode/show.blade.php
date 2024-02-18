4b83b1e19b1f4d05af6be01a16b65227cee06214
shuhei0720 <shuhei_0720@icloud.com>
2024-02-18 13:17:24 +0900

    すべらない話個別表示画面を表示するshow.blade.phpを作成

---
+++ resources/views/episode/show.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            個別表示
+        </h2>
+    </x-slot>
+    <div class="max-w-7xl mx-auto px-6">
+        <div class="bg-white w-full rounded-2xl">
+            <h1 class="text-lg font-semibold">
+                {{$episode->title}}
+            </h1>
+            <hr class="w-full">
+            <p class="mt-4 whitespace-pre-line">
+                {{$episode->episode}}
+            </p>
+            <div class="text-sm font-semibold flex flex-row-reverse">
+                <p> {{$post->created_at}}</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
