fe8bb338c2d324ef6ed57ce463536cb934f7c6a7
shuhei0720 <shuhei_0720@icloud.com>
2024-02-18 13:24:32 +0900

    画面が崩れていたので、show.blade.phpを修正

--- resources/views/episode/show.blade.php
+++ resources/views/episode/show.blade.php
@@ -6,15 +6,17 @@
     </x-slot>
     <div class="max-w-7xl mx-auto px-6">
         <div class="bg-white w-full rounded-2xl">
-            <h1 class="text-lg font-semibold">
-                {{$episode->title}}
-            </h1>
-            <hr class="w-full">
-            <p class="mt-4 whitespace-pre-line">
-                {{$episode->episode}}
-            </p>
-            <div class="text-sm font-semibold flex flex-row-reverse">
-                <p> {{$episode->created_at}}</p>
+            <div class="mt-4 p-4">
+                <h1 class="text-lg font-semibold">
+                    {{$episode->title}}
+                </h1>
+                <hr class="w-full">
+                <p class="mt-4 whitespace-pre-line">
+                    {{$episode->episode}}
+                </p>
+                <div class="text-sm font-semibold flex flex-row-reverse">
+                    <p> {{$episode->created_at}}</p>
+                </div>
             </div>
         </div>
     </div>
