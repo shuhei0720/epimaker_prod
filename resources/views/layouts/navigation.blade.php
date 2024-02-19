1cb23e56a168b1b6e6deb3fb1d6fc961b970216e
shuhei0720 <shuhei_0720@icloud.com>
2024-02-19 12:55:50 +0900

    スマホ用のハンバーガーメニューを追加。また、ロゴ用の画像を配置

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
                         すべらない話一覧
                     </x-nav-link>
                     <x-nav-link :href="route('episode.create')" :active="request()->routeIs('episode.create')">
-                        すべらない話作成画面
+                        すべらない話新規作成
                     </x-nav-link>
                 </div>
             </div>
@@ -70,8 +70,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('episode.index')" :active="request()->routeIs('episode.index')">
+                すべらない話一覧
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('episode.create')" :active="request()->routeIs('episode.create')">
+                すべらない話新規作成
             </x-responsive-nav-link>
         </div>
 
