c849baebcdea5900042ce4d00fb0dc6e7cebe2ff
shuhei0720 <shuhei_0720@icloud.com>
2024-02-19 15:20:52 +0900

    コメントのコントローラーにstoreメソッドを定義

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $inputs=request()->validate([
+            'body'=>'required|max:1000',
+        ]);
+
+        $comment=Comment::create([
+            'body'=>$inputs['body'],
+            'user_id'=>auth()->user()->id,
+            'post_id'=>$request->post_id
+        ]);
+
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
