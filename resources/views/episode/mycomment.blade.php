6577f4eb05ed861d9cb2a803e3e19e63c94c9645
shuhei0720 <shuhei_0720@icloud.com>
2024-02-19 20:05:10 +0900

    mycomment.blade.phpを追加

---
+++ resources/views/episode/mycomment.blade.php
@@ -0,0 +1,53 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold leading-tight text-2xl leading-tight bg-gradient-to-r from-pink-500 via-blue-500 to-green-500 bg-clip-text text-transparent">
+            自分のコメントした投稿一覧
+        </h2>
+    </x-slot>
+    <div class="max-w-7xl mx-auto px-6">
+        @if(session('message'))
+        <div class="text-red-600 font-bold">
+            {{session('message')}}
+        </div>
+        @endif
+        @if(count($episodes)==0)
+        <p class="mt-4">
+            まだコメントした投稿がありません！
+        </p>
+        @else
+        @foreach($episodes as $episode)
+        <div class="mt-4 p-8 bg-yellow-200 w-full rounded-2xl shadow-lg hover:shadow-2xl transition duration-500"">
+            <h1 class="p-4 text-lg font-semibold bg-white border border-gray-400">
+                タイトル：
+                <a href="{{route('episode.show', $episode)}}" class="text-blue-600">
+                {{$episode->title}}
+                </a>
+            </h1>
+            <hr class="w-full">
+            <p class="mt-4 p-4 bg-white border border-gray-400">
+                {{$episode->episode}}
+            </p>
+            <div class="p-4 text-sm font-semibold  bg-white border border-gray-400">
+                <p>
+                    作成者：{{$episode->user->name??'匿名'}} &emsp;  &emsp; {{$episode->created_at->diffForHumans()}}
+                </p>
+            </div>
+            <hr class="w-full mb-2">
+            @if($episode->comments->count())
+            <span class="badge">
+                返信 {{$episode->comments->count()}}件
+            </span>
+            @else
+            <span>コメントはまだありません。</span>
+            @endif
+            <a href="{{route('episode.show', $episode)}}" style="color.white;">
+                <x-primary-button class="float-right">コメントする</x-primary-button>
+            </a>
+        </div>
+        @endforeach
+        <div class="mb-4">
+            {{ $episodes->links() }}
+        </div>
+        @endif
+    </div>
+</x-app-layout>
