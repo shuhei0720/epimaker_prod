5f7617957a29056d0af72f49459e4a4c3584609c
shuhei0720 <shuhei_0720@icloud.com>
2024-02-19 15:07:58 +0900

    コメント用のテーブルを作成し、各モデルとのリレーションを記載

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,19 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Episode extends Model
+class Comment extends Model
 {
     use HasFactory;
 
-    protected $guarded = [
-        'id',
-    ];
+    public function episode() {
+        return $this->belongsTo(Episode::class);
+    }
 
     public function user() {
         return $this->belongsTo(User::class);
-    }
-
-    public function comments() {
-        return $this->hasMany(Comment::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
     public function episodes() {
         return $this->hasMany(Episode::class);
     }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
 }
