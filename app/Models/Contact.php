e8bb2a121bd31ef50fb902cc8db243f979c45570
shuhei0720 <shuhei_0720@icloud.com>
2024-02-20 18:23:08 +0900

    お問い合わせ画面のviewを作成

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers\ContactController;
 
 class Contact extends Model
 {
---
+++ resources/views/contact/create.blade.php
@@ -0,0 +1,35 @@
+<x-guest-layout>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-yellow-50 h-screen">
+        <div class="mx-4 sm:p-8">
+            <h1 class="text-xl text-gray-700 font-semibold hover:underline cursor-pointer">
+                お問い合わせ
+            </h1>
+            <x-message :message="session('message')" />
+
+            <form method="post" action="{{route('contact.store')}}" enctype="multipart/form-data">
+                @csrf
+                <div class="md:flex items-center mt-8">
+                    <div class="w-full flex flex-col">
+                        <label for="title" class="font-semibold leading-none mt-4">件名</label>
+                        <input type="text" name="title" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="title" value="{{old('title')}}" placeholder="Enter Title">
+                    </div>
+                </div>
+
+                <div class="w-full flex flex-col">
+                    <label for="body" class="font-semibold leading-none mt-4">本文</label>
+                    <textarea name="body" class="w-auto py-2 border border-gray-300 rounded-md" id="body" cols="30" rows="10">{{old('body')}}</textarea>
+                </div>
+
+                <div class="md:flex items-center">
+                    <div class="w-full flex flex-col">
+                        <label for="email" class="font-semibold leading-none mt-4">メールアドレス</label>
+                        <input type="text" name="email" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="email" value="{{old('email')}}" placeholder="Enter Email">
+                    </div>
+                </div>
+                <x-primary-button class="mt-4">
+                    送信する
+                </x-primary-button>
+            </form>
+        </div>
+    </div>
+</x-guest-layout>
