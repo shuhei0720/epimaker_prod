0cf26b55d555eeb3b52b818e349c805eef00fa0a
shuhei0720 <shuhei_0720@icloud.com>
2024-02-20 19:30:27 +0900

    RoleモデルとUserモデルのリレーションを追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
     public function comments() {
         return $this->hasMany(Comment::class);
     }
+
+    public function roles() {
+        return $this->belongsToMany(Role::class);
+    }
 }
