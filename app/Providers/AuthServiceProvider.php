f9e6c65e3b59133da237c93cd5a3678e9d9458c9
shuhei0720 <shuhei_0720@icloud.com>
2024-02-20 20:19:21 +0900

    AuthServiceProvider.phpのbootメソッドにadminGateを追加

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +21,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('admin', function($user) {
+            foreach($user->roles as $role) {
+                if($role->name=='admin') {
+                    return true;
+                }
+            }
+            return false;
+        });
     }
 }
