12f77ee214cdcde3a5bdd2505910bc70847b3bd1
shuhei0720 <shuhei_0720@icloud.com>
2024-02-20 23:29:01 +0900

    sendEmailVerificationNotificationメソッドを追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Notifications\NewVerifyEmail;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -53,5 +54,9 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function roles() {
         return $this->belongsToMany(Role::class);
+    }
+
+    public function sendEmailVerificationNotification() {
+        $this->notify(new NewVerifyEmail());
     }
 }
