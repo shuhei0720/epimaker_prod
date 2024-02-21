c6594d801dd13e0ca154f2c7e76656591c8ba7a3
shuhei0720 <shuhei_0720@icloud.com>
2024-02-21 22:03:01 +0900

    Niceモデルを記述

--- app/Models/Nice.php
+++ app/Models/Nice.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Nice extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'user_id',
+        'episode_id'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function episode() {
+        return $this->belongsTo(Episode::class);
+    }
 }
