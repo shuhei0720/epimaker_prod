fc3280b27df789979c81a5de862cdca66dcac1c2
shuhei0720 <shuhei_0720@icloud.com>
2024-02-22 14:24:45 +0900

    Profile Controllerにaddestroyメソッドを記述

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -117,4 +117,14 @@ class ProfileController extends Controller
 
         return Redirect::route('profile.adedit', compact('user'))->with('status', 'profile-updated');
     }
+
+    public function addestroy(User $user) {
+        if($user->avatar!=='user_default.jpg') {
+            $oldavatar='public/avatar/'.$user->avatar;
+            Storage::delete($oldavatar);
+        }
+        $user->roles()->detach();
+        $user->delete();
+        return back()->with('message', 'ユーザーを削除しました');
+    }
 }
