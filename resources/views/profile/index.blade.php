eb1b3906e8249143b655e6fde2b1bce00aece49a
shuhei0720 <shuhei_0720@icloud.com>
2024-02-22 14:22:26 +0900

    ユーザー削除用ボタンを追加

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -15,6 +15,7 @@
                     <th class="p-3 text-left text-white">Email</th>
                     <th class="p-3 text-left text-white">アバター</th>
                     <th class="p-3 text-left text-white">編集</th>
+                    <th class="p-3 text-left text-white">削除</th>
                 </tr>
                 @foreach($users as $user)
                 <tr class="bg-white">
@@ -29,6 +30,13 @@
                     <td class="border-gray-light border hover:bg-gray-100 p-3">
                         <a href="{{route('profile.adedit', $user)}}"><x-primary-button class="bg-teal-700">編集</x-primary-button></a>
                     </td>
+                    <td class="border-gray-light border hover:bg-gray-100 p-3">
+                    <form method="post" action="{{route('profile.addestroy', $user)}}">
+                        @csrf
+                        @method('delete')
+                        <x-primary-button class="bg-red-700" onClick="return confirm('本当に削除しますか？');">削除</x-primary-button>
+                    </form>
+                </td>
                 </tr>
                 @endforeach
             </table>
