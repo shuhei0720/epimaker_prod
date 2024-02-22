2e598b321c6860fc51747589744b0060441ab8db
shuhei0720 <shuhei_0720@icloud.com>
2024-02-22 14:09:35 +0900

    役割付与用のviewファイルを作成

---
+++ resources/views/profile/partials/role-user-form.blade.php
@@ -0,0 +1,37 @@
+<div class="mt-5">
+    <h4 class="mb-3">役割付与・削除（アドミンユーザーにのみ表示）</h4>
+    <table class="text-left w-full border-collapse mt-8">
+        <tr class="bg-green-600 text-center">
+            <th>役割</th>
+            <th>付与</th>
+            <th>削除</th>
+        </tr>
+        @foreach ($roles as $role)
+        <tr class="bg-white text-center">
+            <td class="p-3">
+                {{$role->name}}
+            </td>
+            <td class="p-3">
+                <form method="post" action="{{route('role.attach', $user)}}">
+                    @csrf
+                    @method('patch')
+                    <input type="hidden" name="role" value="{{$role->id}}">
+                    <button class="btnroleb">
+                        役割付与
+                    </button>
+                </form>
+            </td>
+            <td class="p-3">
+                <form method="post" action="{{route('role.detach', $user)}}">
+                    @csrf
+                    @method('patch')
+                    <input type="hidden" name="role" value="{{$role->id}}">
+                    <button class="btnroler">
+                        役割削除
+                    </button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+   </table>
+</div>
