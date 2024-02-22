a21d91bb66113005053fa371d03e4d0210eecaab
shuhei0720 <shuhei_0720@icloud.com>
2024-02-22 14:18:09 +0900

    管理者のユーザー編集画面にのみ役割付与テーブルが表示されるよう修正

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -19,11 +19,13 @@
                 </div>
             </div>
 
+            @if(isset($admin))
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.role-user-form')
                 </div>
             </div>
+            @endif
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
