c26766cdbad640c347f626454b72e4958f4982bb
shuhei0720 <shuhei_0720@icloud.com>
2024-02-22 13:32:24 +0900

    管理者用のユーザー編集ボタンを配置

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -13,12 +13,22 @@
                     <th class="p-3 text-left text-white">＃</th>
                     <th class="p-3 text-left text-white">名前</th>
                     <th class="p-3 text-left text-white">Email</th>
+                    <th class="p-3 text-left text-white">アバター</th>
+                    <th class="p-3 text-left text-white">編集</th>
                 </tr>
                 @foreach($users as $user)
                 <tr class="bg-white">
                     <td class="border-gray-light border hover:bg-gray-100 p-3">{{$user->id}}</td>
                     <td class="border-gray-light border hover:bg-gray-100 p-3">{{$user->name}}</td>
                     <td class="border-gray-light border hover:bg-gray-100 p-3">{{$user->email}}</td>
+                    <td class="border-gray-light border hover:bg-gray-100 p-3">
+                        <div class="rounded-full w-12 h-12">
+                            <img src="{{asset('storage/avatar/'.($user->avatar??'user_default.jpg'))}}">
+                        </div>
+                    </td>
+                    <td class="border-gray-light border hover:bg-gray-100 p-3">
+                        <a href="{{route('profile.adedit', $user)}}"><x-primary-button class="bg-teal-700">編集</x-primary-button></a>
+                    </td>
                 </tr>
                 @endforeach
             </table>
