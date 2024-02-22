158020e425312e50b29911fb59413b6f0f0cf15a
shuhei0720 <shuhei_0720@icloud.com>
2024-02-22 12:34:03 +0900

    プロフィール編集画面にアバター画像登録フォームを追加

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,7 +13,7 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
         @csrf
         @method('patch')
 
@@ -45,6 +45,15 @@
                     @endif
                 </div>
             @endif
+        </div>
+
+        <div>
+            <x-input-label for="avatar" :value="__('プロフィール画像（任意・1MBまで）')" />
+            <div class="rounded-full w-36">
+                <img src="{{asset('storage/avatar/'.($user->avatar??'user_default.jpg'))}}">
+            </div>
+            <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" :value="old('avatar')" />
+            <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
         </div>
 
         <div class="flex items-center gap-4">
