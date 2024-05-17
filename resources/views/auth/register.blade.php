63b900b545d2c36ec9a84c8283f2a106d07c013f
EC2 Default User <ec2-user@ip-10-0-5-163.ap-northeast-1.compute.internal>
2024-05-17 17:13:39 +0000

    細かい修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,7 +25,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('パスワード (8文字以上)')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
